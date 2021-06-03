fd0cfb96ecc13e34c6e67acda0d8d8d0681f82c6
Ymahlawat <69887149+Ymahlawat@users.noreply.github.com>
2021-06-03 16:49:48 +0530

    js update

---
+++ OnlineMeal/jasc.js
@@ -0,0 +1,15 @@
+
+
+  document.getElementById('icon-to').addEventListener("click", function() {
+  
+    var x = document.getElementById('nav-items');
+  
+    if(x.style.display=="none"){
+     x.style.display="block";
+  }else{
+    x.style.display= "none";
+  }})
+
+
+
+
